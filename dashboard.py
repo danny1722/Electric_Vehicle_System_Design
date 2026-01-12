57e19cd5ef73fac54490e3f1fa12f947b68cab4c
N-Schaap <nick.schaap2000@outlook.com>
2026-01-12 14:30:46 +0100

    python html integration

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request 
-app = Flask(__name__) 
-@app.route('/form', methods=['GET', 'POST']) 
-def form_view(): 
-  if request.method == 'POST': # Process form data 
-    pass 
-  return render_template('form.html')
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+
+def submit():
+    if request.method == 'POST':
+        batteryCapacity = request.form['battery-capacity']
+        return render_template('main.html', batteryCapacity=batteryCapacity)
+    return render_template('main.html', batteryCapacity=batteryCapacity)
+
+if __name__ == '__main__':
+    batteryCapacity = 0
+    app.run(debug=True)
+
